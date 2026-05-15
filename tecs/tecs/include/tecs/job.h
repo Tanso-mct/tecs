43c095f36900e68f2ff5400bc7cb94cd4c17c959
Tanso <tansomctexp@gmail.com>
2026-05-15 10:52:04 +0900

    Currently refactoring Job.

--- tecs/tecs/include/tecs/job.h
+++ tecs/tecs/include/tecs/job.h
@@ -1,298 +1,368 @@
 ﻿#pragma once
 
 // STL
-#include <functional>
-#include <condition_variable>
-#include <atomic>
-#include <mutex>
+#include <string>
+#include <string_view>
 #include <memory>
-#include <queue>
-#include <thread>
-#include <vector>
 
 namespace tecs
 {
 
-class Job
-{
-
-};
-
-class WorkerThread
+class JobType
 {
 public:
-    WorkerThread() = default;
-    ~WorkerThread() = default;
+    /**
+     * @brief : Construct a new JobType object with the specified parameters
+     * @param name : The name of the job type
+     * @param id : A unique identifier for the job type
+     * @param priority : The priority level of the job type
+     * @param cost : An estimate of the computational cost of jobs of this type
+     */
+    JobType(std::string name, uint32_t id, uint32_t priority, uint32_t cost);
+
+    ~JobType() = default;
 
     /**
-     * @brief
-     * Start the worker thread to execute the given function
-     * 
-     * @param func
-     * The function to be executed in the worker thread
+     * @brief : Get the name of the job type
+     * @return std::string_view : The name of the job type
      */
-    void Start(std::function<void()> func);
+    std::string_view GetName() const { return name_; }
 
     /**
-     * @brief
-     * Stop the worker thread and wait for it to finish execution
+     * @brief : Get the unique identifier of the job type
+     * @return uint32_t : The unique identifier of the job type
      */
-    void Stop();
+    uint32_t GetID() const { return id_; }
 
     /**
-     * @brief
-     * Check if the worker thread is currently running (executing a job)
-     * 
-     * @return true
-     * If the worker thread is currently executing a job
-     * 
-     * @return false
-     * If the worker thread is idle (not executing a job)
+     * @brief : Get the priority level of the job type
+     * @return uint32_t : The priority level of the job type
      */
-    bool IsRunning() const;
+    uint32_t GetPriority() const { return priority_; }
 
     /**
-     * @brief
-     * Dispatch a job to the worker thread for execution
-     * 
-     * @param job
-     * The job to be executed by the worker thread
-     * 
-     * @return bool 
+     * @brief : Get the estimated computational cost of jobs of this type
+     * @return uint32_t : The estimated computational cost of jobs of this type
      */
-    bool DispatchJob(std::unique_ptr<Job> job);
-
-
-};
-
-
-class ThreadProvider
-{
-public:
-    ThreadProvider() = default;
-    ~ThreadProvider() = default;
-
-    /**
-     * @brief
-     * Get the number of CPU cores available
-     * 
-     * @return uint32_t
-     * The number of CPU cores
-     */
-    uint32_t GetCpuCoreCount();
-
-    /**
-     * @brief
-     * Create a new thread to execute the given function
-     * 
-     * @param func
-     * The function to be executed in the new thread
-     */
-    void CreateThread(std::function<void()> func);
-
-    /**
-     * @brief
-     * Create multiple threads to execute the given function
-     * 
-     * @param func
-     * The function to be executed in each new thread
-     * 
-     * @param num_threads
-     * The number of threads to create
-     */
-    void CreateThreads(std::function<void()> func, uint32_t num_threads);
-
-
+    uint32_t GetCost() const { return cost_; }
 
 private:
-    // Vector to hold created threads
-    std::vector<std::thread> threads_;
+    // The name of the job type, used for identification and categorization
+    std::string name_;
+
+    // A unique identifier for the job type, used for efficient lookup and management
+    uint32_t id_;
+
+    // The priority level of the job type, which can be used to determine the order of job execution
+    uint32_t priority_;
+
+    // An estimate of the computational cost
+    uint32_t cost_;
 };
 
-// Forward declaration
-class JobHandle;
-
-/**
- * @brief
- * Class representing the state of a job
- * This class is used internally by JobHandle to manage job completion
- */
 class JobState
 {
 public:
-    JobState() = default;
+    /**
+     * @brief : Construct a new JobState object with the specified job type and completion status
+     * @param is_completed : A weak pointer to a boolean that indicates whether the job has been completed
+     */
+    JobState(std::weak_ptr<bool> is_completed);
+
     ~JobState() = default;
 
-    // Allow JobHandle to access private members
-    friend class JobHandle;
+    /**
+     * @brief : Check if the job has been completed
+     * @return bool : True if the job is completed, false otherwise
+     */
+    bool IsCompleted() const;
 
     /**
-     * @brief
-     * Mark the job as completed and notify all waiting threads
+     * @brief : Wait for the job to be completed and return the completion status
+     * @return bool : True if the job is completed, false otherwise
      */
-    void MarkCompleted();
+    bool WaitForCompletion() const;
 
 private:
-    // Atomic flag to indicate job completion
-    std::atomic<bool> completed_{false};
-
-    // Condition variable to notify waiting threads
-    std::condition_variable cv_ = {};
-
-    // Mutex for synchronizing access to the condition variable
-    std::mutex mutex_ = {};
+    // A weak pointer to a boolean that indicates whether the job has been completed
+    std::weak_ptr<bool> is_completed_;
 };
 
-/**
- * @brief
- * Handle for a scheduled job
- * Allows waiting for job completion
- */
-class JobHandle
-{
-public:
-    /**
-     * @brief
-     * Construct a new Job Handle object
-     * 
-     * @param state
-     * Shared pointer to the JobState
-     */
-    JobHandle(std::shared_ptr<JobState> state);
+// class WorkerThread
+// {
+// public:
+//     WorkerThread() = default;
+//     ~WorkerThread() = default;
 
-    ~JobHandle() = default;
+//     /**
+//      * @brief
+//      * Start the worker thread to execute the given function
+//      * 
+//      * @param func
+//      * The function to be executed in the worker thread
+//      */
+//     void Start(std::function<void()> func);
 
-    /**
-     * @brief
-     * Wait for the job to complete
-     * This function blocks until the job is marked as completed
-     */
-    void Wait();
+//     /**
+//      * @brief
+//      * Stop the worker thread and wait for it to finish execution
+//      */
+//     void Stop();
 
-private:
-    // Shared pointer to the JobState
-    std::shared_ptr<JobState> state_ = nullptr;
-};
+//     /**
+//      * @brief
+//      * Check if the worker thread is currently running (executing a job)
+//      * 
+//      * @return true
+//      * If the worker thread is currently executing a job
+//      * 
+//      * @return false
+//      * If the worker thread is idle (not executing a job)
+//      */
+//     bool IsRunning() const;
 
-/**
- * @brief
- * Job class representing a unit of work to be executed
- * This class encapsulates a function to be run as a job
- */
-class Job
-{
-public:
-    /**
-     * @brief
-     * Alias for the job function
-     */
-    using JobFunc = std::function<void()>;
+//     /**
+//      * @brief
+//      * Dispatch a job to the worker thread for execution
+//      * 
+//      * @param job
+//      * The job to be executed by the worker thread
+//      * 
+//      * @return bool 
+//      */
+//     bool DispatchJob(std::unique_ptr<Job> job);
 
-    /**
-     * @brief
-     * Construct a new Job object
-     * 
-     * @param func 
-     * The function to be executed as part of the job
-     * If nullptr is passed, will be asserted
-     */
-    Job(JobFunc func);
+
+// };
+
+
+// class ThreadProvider
+// {
+// public:
+//     ThreadProvider() = default;
+//     ~ThreadProvider() = default;
+
+//     /**
+//      * @brief
+//      * Get the number of CPU cores available
+//      * 
+//      * @return uint32_t
+//      * The number of CPU cores
+//      */
+//     uint32_t GetCpuCoreCount();
+
+//     /**
+//      * @brief
+//      * Create a new thread to execute the given function
+//      * 
+//      * @param func
+//      * The function to be executed in the new thread
+//      */
+//     void CreateThread(std::function<void()> func);
+
+//     /**
+//      * @brief
+//      * Create multiple threads to execute the given function
+//      * 
+//      * @param func
+//      * The function to be executed in each new thread
+//      * 
+//      * @param num_threads
+//      * The number of threads to create
+//      */
+//     void CreateThreads(std::function<void()> func, uint32_t num_threads);
+
+
+
+// private:
+//     // Vector to hold created threads
+//     std::vector<std::thread> threads_;
+// };
+
+// // Forward declaration
+// class JobHandle;
+
+// /**
+//  * @brief
+//  * Class representing the state of a job
+//  * This class is used internally by JobHandle to manage job completion
+//  */
+// class JobState
+// {
+// public:
+//     JobState() = default;
+//     ~JobState() = default;
+
+//     // Allow JobHandle to access private members
+//     friend class JobHandle;
+
+//     /**
+//      * @brief
+//      * Mark the job as completed and notify all waiting threads
+//      */
+//     void MarkCompleted();
+
+// private:
+//     // Atomic flag to indicate job completion
+//     std::atomic<bool> completed_{false};
+
+//     // Condition variable to notify waiting threads
+//     std::condition_variable cv_ = {};
+
+//     // Mutex for synchronizing access to the condition variable
+//     std::mutex mutex_ = {};
+// };
+
+// /**
+//  * @brief
+//  * Handle for a scheduled job
+//  * Allows waiting for job completion
+//  */
+// class JobHandle
+// {
+// public:
+//     /**
+//      * @brief
+//      * Construct a new Job Handle object
+//      * 
+//      * @param state
+//      * Shared pointer to the JobState
+//      */
+//     JobHandle(std::shared_ptr<JobState> state);
+
+//     ~JobHandle() = default;
+
+//     /**
+//      * @brief
+//      * Wait for the job to complete
+//      * This function blocks until the job is marked as completed
+//      */
+//     void Wait();
+
+// private:
+//     // Shared pointer to the JobState
+//     std::shared_ptr<JobState> state_ = nullptr;
+// };
+
+// /**
+//  * @brief
+//  * Job class representing a unit of work to be executed
+//  * This class encapsulates a function to be run as a job
+//  */
+// class Job
+// {
+// public:
+//     /**
+//      * @brief
+//      * Alias for the job function
+//      */
+//     using JobFunc = std::function<void()>;
+
+//     /**
+//      * @brief
+//      * Construct a new Job object
+//      * 
+//      * @param func 
+//      * The function to be executed as part of the job
+//      * If nullptr is passed, will be asserted
+//      */
+//     Job(JobFunc func);
     
-    ~Job() = default;
+//     ~Job() = default;
 
-    /**
-     * @brief
-     * Execute the job function
-     */
-    void Execute();
+//     /**
+//      * @brief
+//      * Execute the job function
+//      */
+//     void Execute();
 
-private:
-    // The function to be executed as part of the job
-    JobFunc func_ = nullptr;
-};
+// private:
+//     // The function to be executed as part of the job
+//     JobFunc func_ = nullptr;
+// };
 
-/**
- * @brief
- * Struct representing a scheduled job along with its state
- */
-class JobSet
-{
-public:
-    /**
-     * @brief
-     * Construct a new Job Set object
-     * 
-     * @param job 
-     * The job to be scheduled
-     * 
-     * @param state 
-     * Shared pointer to the JobState
-     */
-    JobSet(Job job, std::shared_ptr<JobState> state);
+// /**
+//  * @brief
+//  * Struct representing a scheduled job along with its state
+//  */
+// class JobSet
+// {
+// public:
+//     /**
+//      * @brief
+//      * Construct a new Job Set object
+//      * 
+//      * @param job 
+//      * The job to be scheduled
+//      * 
+//      * @param state 
+//      * Shared pointer to the JobState
+//      */
+//     JobSet(Job job, std::shared_ptr<JobState> state);
 
-    Job job_;
-    std::shared_ptr<JobState> state_;
-};
+//     Job job_;
+//     std::shared_ptr<JobState> state_;
+// };
 
-/**
- * @brief 
- * Job Scheduler class responsible for managing job execution
- * Currently a placeholder with no implemented functionality
- */
-class JobScheduler
-{
-public:
-    /**
-     * @brief 
-     * Construct a new Job Scheduler object
-     * Worker thread is started upon construction
-     * 
-     * @param num_worker_threads
-     * Number of worker threads to process jobs
-     */
-    JobScheduler(uint32_t num_worker_threads);
+// /**
+//  * @brief 
+//  * Job Scheduler class responsible for managing job execution
+//  * Currently a placeholder with no implemented functionality
+//  */
+// class JobScheduler
+// {
+// public:
+//     /**
+//      * @brief 
+//      * Construct a new Job Scheduler object
+//      * Worker thread is started upon construction
+//      * 
+//      * @param num_worker_threads
+//      * Number of worker threads to process jobs
+//      */
+//     JobScheduler(uint32_t num_worker_threads);
 
-    /**
-     * @brief
-     * Destroy the Job Scheduler object
-     * Worker thread is stopped and joined upon destruction
-     */
-    ~JobScheduler();
+//     /**
+//      * @brief
+//      * Destroy the Job Scheduler object
+//      * Worker thread is stopped and joined upon destruction
+//      */
+//     ~JobScheduler();
 
-    /**
-     * @brief
-     * Schedule a job for execution
-     * 
-     * @param job
-     * The job to be scheduled
-     * 
-     * @return JobHandle
-     * Handle to the scheduled job
-     * You can use this handle to wait for job completion
-     */
-    JobHandle ScheduleJob(Job job);
+//     /**
+//      * @brief
+//      * Schedule a job for execution
+//      * 
+//      * @param job
+//      * The job to be scheduled
+//      * 
+//      * @return JobHandle
+//      * Handle to the scheduled job
+//      * You can use this handle to wait for job completion
+//      */
+//     JobHandle ScheduleJob(Job job);
 
-private:
-    // Queue to hold scheduled jobs
-    std::queue<std::unique_ptr<JobSet>> job_queue_;
+// private:
+//     // Queue to hold scheduled jobs
+//     std::queue<std::unique_ptr<JobSet>> job_queue_;
 
-    // Mutex for thread-safe access to the job queue
-    std::mutex mutex_;
+//     // Mutex for thread-safe access to the job queue
+//     std::mutex mutex_;
 
-    // Condition variable to notify worker thread of new jobs
-    std::condition_variable cv_;
+//     // Condition variable to notify worker thread of new jobs
+//     std::condition_variable cv_;
 
-    // Atomic flag to signal the worker thread to stop
-    std::atomic<bool> stop_flag_{false};
+//     // Atomic flag to signal the worker thread to stop
+//     std::atomic<bool> stop_flag_{false};
 
-    // Worker threads for processing jobs
-    std::vector<std::thread> worker_threads_;
+//     // Worker threads for processing jobs
+//     std::vector<std::thread> worker_threads_;
 
-    /**
-     * @brief
-     * Worker function that processes jobs from the queue
-     */
-    void Work();
-};
+//     /**
+//      * @brief
+//      * Worker function that processes jobs from the queue
+//      */
+//     void Work();
+// };
 
 } // namespace tecs
--- tecs/tecs/src/job.cpp
+++ tecs/tecs/src/job.cpp
@@ -4,117 +4,147 @@
 namespace tecs
 {
 
-void JobState::MarkCompleted()
-{
-    {
-        std::lock_guard<std::mutex> lock(mutex_);
-        completed_.store(true);
-    }
-
-    // Notify all waiting threads
-    cv_.notify_all();
-}
-
-JobHandle::JobHandle(std::shared_ptr<JobState> state) : 
-    state_(state)
+JobType::JobType(std::string name, uint32_t id, uint32_t priority, uint32_t cost) :
+    name_(std::move(name)),
+    id_(id),
+    priority_(priority),
+    cost_(cost)
 {
 }
 
-void JobHandle::Wait()
+JobState::JobState(std::weak_ptr<bool> is_completed) :
+    is_completed_(std::move(is_completed))
 {
-    assert(state_ != nullptr && "JobState must not be nullptr");
-
-    std::unique_lock<std::mutex> lock(state_->mutex_);
-    state_->cv_.wait(lock, [this]() { return state_->completed_.load(); });
+    assert(!is_completed_.expired() && "is_completed weak pointer must not be expired");
 }
 
-Job::Job(JobFunc func)
-    : func_(std::move(func))
+bool JobState::IsCompleted() const
 {
+    // Attempt to lock the weak pointer to get a shared pointer
+    if (std::shared_ptr<bool> is_completed_shared = is_completed_.lock())
+        return *is_completed_shared;
+
+    // If the weak pointer cannot be locked, it means the job state has been destroyed
+    // In this case, we can consider the job as completed
+    return true;
 }
 
-void Job::Execute()
+bool JobState::WaitForCompletion() const
 {
-    assert(func_ != nullptr && "Job function must not be nullptr");
-    func_();
+    
 }
 
-JobSet::JobSet(Job job, std::shared_ptr<JobState> state) :
-    job_(std::move(job)), 
-    state_(std::move(state))
-{
-}
+// void JobState::MarkCompleted()
+// {
+//     {
+//         std::lock_guard<std::mutex> lock(mutex_);
+//         completed_.store(true);
+//     }
 
-JobScheduler::JobScheduler(uint32_t num_worker_threads) :
-    stop_flag_(false)
-{
-    // Start the specified number of worker threads
-    for (uint32_t i = 0; i < num_worker_threads; ++i)
-        worker_threads_.emplace_back(&JobScheduler::Work, this);
-}
+//     // Notify all waiting threads
+//     cv_.notify_all();
+// }
 
-JobScheduler::~JobScheduler()
-{
-    // Signal the worker thread to stop
-    stop_flag_.store(true);
-    cv_.notify_all();
+// JobHandle::JobHandle(std::shared_ptr<JobState> state) : 
+//     state_(state)
+// {
+// }
 
-    // Join the worker threads
-    for (std::thread& thread : worker_threads_)
-        if (thread.joinable())
-            thread.join();
-}
+// void JobHandle::Wait()
+// {
+//     assert(state_ != nullptr && "JobState must not be nullptr");
 
-JobHandle JobScheduler::ScheduleJob(Job job)
-{
-    // Create a new JobState for the scheduled job
-    std::shared_ptr<JobState> state = std::make_shared<JobState>();
+//     std::unique_lock<std::mutex> lock(state_->mutex_);
+//     state_->cv_.wait(lock, [this]() { return state_->completed_.load(); });
+// }
 
-    {
-        // Lock the job queue and condition variable
-        std::lock_guard<std::mutex> lock(mutex_);
+// Job::Job(JobFunc func)
+//     : func_(std::move(func))
+// {
+// }
 
-        // Add the job and its state to the queue
-        job_queue_.push(std::make_unique<JobSet>(std::move(job), state));
+// void Job::Execute()
+// {
+//     assert(func_ != nullptr && "Job function must not be nullptr");
+//     func_();
+// }
 
-        // Notify the worker thread of the new job
-        cv_.notify_one();
-    }
+// JobSet::JobSet(Job job, std::shared_ptr<JobState> state) :
+//     job_(std::move(job)), 
+//     state_(std::move(state))
+// {
+// }
 
-    // Return a JobHandle associated with the job's state
-    return JobHandle(state);
-}
+// JobScheduler::JobScheduler(uint32_t num_worker_threads) :
+//     stop_flag_(false)
+// {
+//     // Start the specified number of worker threads
+//     for (uint32_t i = 0; i < num_worker_threads; ++i)
+//         worker_threads_.emplace_back(&JobScheduler::Work, this);
+// }
 
-void JobScheduler::Work()
-{
-    // Continuously process jobs until stop flag is set
-    while (!stop_flag_.load())
-    {
-        // JobSet
-        std::unique_ptr<JobSet> job_set = nullptr;
+// JobScheduler::~JobScheduler()
+// {
+//     // Signal the worker thread to stop
+//     stop_flag_.store(true);
+//     cv_.notify_all();
 
-        {
-            std::unique_lock<std::mutex> lock(mutex_);
+//     // Join the worker threads
+//     for (std::thread& thread : worker_threads_)
+//         if (thread.joinable())
+//             thread.join();
+// }
 
-            // Wait for a job to be available or for the stop flag to be set
-            cv_.wait(lock, [this]() { return !job_queue_.empty() || stop_flag_.load(); });
+// JobHandle JobScheduler::ScheduleJob(Job job)
+// {
+//     // Create a new JobState for the scheduled job
+//     std::shared_ptr<JobState> state = std::make_shared<JobState>();
 
-            if (stop_flag_.load())
-                break; // Exit if stop flag is set
+//     {
+//         // Lock the job queue and condition variable
+//         std::lock_guard<std::mutex> lock(mutex_);
 
-            // Get job set from the queue
-            job_set = std::move(job_queue_.front());
+//         // Add the job and its state to the queue
+//         job_queue_.push(std::make_unique<JobSet>(std::move(job), state));
 
-            // Remove the job set from the queue
-            job_queue_.pop();
-        }
+//         // Notify the worker thread of the new job
+//         cv_.notify_one();
+//     }
 
-        // Execute the job
-        job_set->job_.Execute();
+//     // Return a JobHandle associated with the job's state
+//     return JobHandle(state);
+// }
 
-        // Mark the job as completed
-        job_set->state_->MarkCompleted();
-    }
-}
+// void JobScheduler::Work()
+// {
+//     // Continuously process jobs until stop flag is set
+//     while (!stop_flag_.load())
+//     {
+//         // JobSet
+//         std::unique_ptr<JobSet> job_set = nullptr;
+
+//         {
+//             std::unique_lock<std::mutex> lock(mutex_);
+
+//             // Wait for a job to be available or for the stop flag to be set
+//             cv_.wait(lock, [this]() { return !job_queue_.empty() || stop_flag_.load(); });
+
+//             if (stop_flag_.load())
+//                 break; // Exit if stop flag is set
+
+//             // Get job set from the queue
+//             job_set = std::move(job_queue_.front());
+
+//             // Remove the job set from the queue
+//             job_queue_.pop();
+//         }
+
+//         // Execute the job
+//         job_set->job_.Execute();
+
+//         // Mark the job as completed
+//         job_set->state_->MarkCompleted();
+//     }
+// }
 
 } // namespace tecs
