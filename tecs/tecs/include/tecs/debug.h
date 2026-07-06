6ca827563fdb88dc0ff2721f0262f7e726937aa6
Tanso <tansomctexp@gmail.com>
2026-07-06 13:11:12 +0900

    Implement the debug functions using PlaceHolder.

--- tecs/tecs/include/tecs/debug.h
+++ tecs/tecs/include/tecs/debug.h
@@ -1,125 +1,181 @@
-﻿@startuml
+﻿#pragma once
+
+// STL
+#include <string>
+#include <string_view>
+#include <memory>
+
+// TECS
+#include "tecs/job.h"
+#include "tecs/system.h"
+#include "tecs/registry.h"
+
+namespace tecs
+{
 
 class LogExporter
 {
-    + virtual ~LogExporter() = default
+public:
+    virtual ~LogExporter() = default;
 
-    + virtual bool Analyze() const = 0
+    /**
      * @brief : Analyzes the log data and returns true if the analysis is successful, false otherwise
      * @return bool : Returns true if the log analysis is successful, false otherwise
+     */
+    virtual bool Analyze() const = 0;
 
-    + virtual std::unique_ptr<uint8_t[]> Export(uint32_t& size) const = 0
+    /**
      * @brief : Exports the log data to a byte array
      * @param size : Reference to a variable that will hold the size of the exported log data
      * @return std::unique_ptr<uint8_t[]> : Returns a unique pointer to a byte array containing the exported log data
-    ---
-}
+     */
+    virtual std::unique_ptr<uint8_t[]> Export(uint32_t& size) const = 0;
+
+};
 
 class JobLogExporter
+    : public LogExporter
 {
-    + JobLogExporter(JobTracker& job_tracker)
+public:
+    /**
      * @brief : Constructor for JobLogExporter
      * @param job_tracker : Reference to a JobTracker object
+     */
+    JobLogExporter(JobTracker& job_tracker);
 
-    + bool Analyze() const override
+    /**
      * @brief : Analyzes the job log data and returns true if the analysis is successful, false otherwise
      * @return bool : Returns true if the job log analysis is successful, false otherwise
+     */
+    bool Analyze() const override;
 
-    + std::unique_ptr<uint8_t[]> Export(uint32_t& size) const override
+    /**
      * @brief : Exports the job log data to a byte array
      * @param size : Reference to a variable that will hold the size of the exported job log data
      * @return std::unique_ptr<uint8_t[]> : Returns a unique pointer to a byte array containing the exported job log data
-    ---
-    - JobTracker& job_tracker_
-     * Reference to the JobTracker object used for exporting job logs
-}
-JobLogExporter --|> LogExporter
-JobLogExporter --> JobTracker
+     */
+    std::unique_ptr<uint8_t[]> Export(uint32_t& size) const override;
+
+private:
+    // Reference to the JobTracker object used for exporting job logs
+    JobTracker& job_tracker_;
+
+};
 
 class SystemLogExporter
+    : public LogExporter
 {
-    + SystemLogExporter(std::vector<std::unique_ptr<SystemView>>&& system_views)
+public:
+    /**
      * @brief : Constructor for SystemLogExporter
      * @param system_views : A vector of unique pointers to SystemView objects
+     */
+    SystemLogExporter(std::vector<std::unique_ptr<SystemView>>&& system_views);
 
-    + bool Analyze() const override
+    /**
      * @brief : Analyzes the system log data and returns true if the analysis is successful, false otherwise
      * @return bool : Returns true if the system log analysis is successful, false otherwise
+     */
+    bool Analyze() const override;
 
-    + std::unique_ptr<uint8_t[]> Export(uint32_t& size) const override
+    /**
      * @brief : Exports the system log data to a byte array
      * @param size : Reference to a variable that will hold the size of the exported system log data
      * @return std::unique_ptr<uint8_t[]> : Returns a unique pointer to a byte array containing the exported system log data
-    ---
-    - std::vector<std::unique_ptr<SystemView>> system_views_
-     * A vector of unique pointers to SystemView objects used for exporting system logs
-}
-SystemLogExporter --|> LogExporter
-SystemLogExporter --> SystemView
+     */
+    std::unique_ptr<uint8_t[]> Export(uint32_t& size) const override;
+
+private:
+    // A vector of unique pointers to SystemView objects used for exporting system logs
+    std::vector<std::unique_ptr<SystemView>> system_views_;
+
+};
 
 class EntityLogExporter
+    : public LogExporter
 {
-    + EntityLogExporter(Registry& registry)
+public:
+    /**
      * @brief : Constructor for EntityLogExporter
      * @param registry : Reference to a Registry object
+     */
+    EntityLogExporter(Registry& registry);
 
-    + bool Analyze() const override
+    /**
      * @brief : Analyzes the entity log data and returns true if the analysis is successful, false otherwise
      * @return bool : Returns true if the entity log analysis is successful, false otherwise
+     */
+    bool Analyze() const override;
 
-    + std::unique_ptr<uint8_t[]> Export(uint32_t& size) const override
+    /**
      * @brief : Exports the entity log data to a byte array
      * @param size : Reference to a variable that will hold the size of the exported entity log data
      * @return std::unique_ptr<uint8_t[]> : Returns a unique pointer to a byte array containing the exported entity log data
-    ---
-    - Registry& registry_
-     * Reference to the Registry object used for exporting entity logs
-}
-EntityLogExporter --|> LogExporter
-EntityLogExporter --> Registry
+     */
+    std::unique_ptr<uint8_t[]> Export(uint32_t& size) const override;
+
+private:
+    // Reference to the Registry object used for exporting entity logs
+    Registry& registry_;
+
+};
 
 class Debug
 {
-    + Debug(JobScheduler& job_scheduler)
+public:
+    /**
      * @brief : Constructor for Debug class
      * @param job_scheduler : Reference to a JobScheduler object
-    
-    + ~Debug() = default
+     */
+    Debug(JobScheduler& job_scheduler);
 
-    + void AddLogExporter(uint32_t exporter_id, std::unique_ptr<LogExporter> exporter)
+    ~Debug() = default;
+
+    /**
      * @brief : Adds a log exporter to the Debug class
      * @param exporter_id : The ID of the log exporter
      * @param exporter : A unique pointer to the LogExporter instance to be added
+     */
+    void AddLogExporter(uint32_t exporter_id, std::unique_ptr<LogExporter> exporter);
 
-    + bool ExportLog(uint32_t exporter_id, std::string_view output_file) const
+    /**
      * @brief : Exports the log data using the specified log exporter
      * @param exporter_id : The ID of the log exporter to be used for exporting
      * @param output_file : The path to the output file where the exported log data will be saved
      * @return bool : Returns true if the export was successful, false otherwise
-    ---
-    - JobScheduler& job_scheduler_
-     * Reference to the JobScheduler object used for managing jobs
+     */
+    bool ExportLog(uint32_t exporter_id, std::string_view output_file) const;
 
-    - std::unordered_map<uint32_t, std::unique_ptr<LogExporter>> log_exporters_
-     * A map that associates log exporter IDs with their corresponding LogExporter instances
-}
-Debug *-- LogExporter
+private:
+    // Reference to the JobScheduler object used for managing jobs
+    JobScheduler& job_scheduler_;
+
+    // A map that associates log exporter IDs with their corresponding LogExporter instances
+    std::unordered_map<uint32_t, std::unique_ptr<LogExporter>> log_exporters_;
+
+};
 
 class DebugLog
 {
-    + DebugLog(Debug& debug)
+public:
+    /**
      * @brief : Constructor for DebugLog class
      * @param debug : Reference to a Debug object
+     */
+    DebugLog(Debug& debug);
 
-    + bool ExportLog(uint32_t exporter_id, std::string_view output_file) const
+    /**
      * @brief : Exports the log data using the specified log exporter
      * @param exporter_id : The ID of the log exporter to be used for exporting
      * @param output_file : The path to the output file where the exported log data will be saved
      * @return bool : Returns true if the export was successful, false otherwise
-    ---
-    - Debug& debug_
-     * Reference to the Debug object used for exporting logs
-}
-DebugLog --> Debug
+     */
+    bool ExportLog(uint32_t exporter_id, std::string_view output_file) const;
 
-@enduml
+private:
+    // Reference to the Debug object used for exporting logs
+    Debug& debug_;
+
+};
+
+} // namespace tecs
---
+++ tecs/tecs/src/debug.cpp
@@ -0,0 +1,82 @@
+﻿#include "pch.h"
+#include "tecs/debug.h"
+
+namespace tecs
+{
+
+JobLogExporter::JobLogExporter(JobTracker& job_tracker)
+    : job_tracker_(job_tracker)
+{
+}
+
+bool JobLogExporter::Analyze() const
+{
+    return true;
+}
+
+std::unique_ptr<uint8_t[]> JobLogExporter::Export(uint32_t& size) const
+{
+    size = 0;
+    return std::make_unique<uint8_t[]>(size);
+}
+
+SystemLogExporter::SystemLogExporter(std::vector<std::unique_ptr<SystemView>>&& system_views)
+    : system_views_(std::move(system_views))
+{
+}
+
+bool SystemLogExporter::Analyze() const
+{
+    return true;
+}
+
+std::unique_ptr<uint8_t[]> SystemLogExporter::Export(uint32_t& size) const
+{
+    size = 0;
+    return std::make_unique<uint8_t[]>(size);
+}
+
+EntityLogExporter::EntityLogExporter(Registry& registry)
+    : registry_(registry)
+{
+}
+
+bool EntityLogExporter::Analyze() const
+{
+    return true;
+}
+
+std::unique_ptr<uint8_t[]> EntityLogExporter::Export(uint32_t& size) const
+{
+    size = 0;
+    return std::make_unique<uint8_t[]>(size);
+}
+
+Debug::Debug(JobScheduler& job_scheduler)
+    : job_scheduler_(job_scheduler)
+{
+}
+
+void Debug::AddLogExporter(uint32_t exporter_id, std::unique_ptr<LogExporter> exporter)
+{
+    assert(exporter != nullptr && "LogExporter pointer cannot be null");
+    assert(log_exporters_.find(exporter_id) == log_exporters_.end() && "LogExporter with the same ID already exists");
+    log_exporters_.emplace(exporter_id, std::move(exporter));
+}
+
+bool Debug::ExportLog(uint32_t exporter_id, std::string_view output_file) const
+{
+    return true;
+}
+
+DebugLog::DebugLog(Debug& debug)
+    : debug_(debug)
+{
+}
+
+bool DebugLog::ExportLog(uint32_t exporter_id, std::string_view output_file) const
+{
+    return debug_.ExportLog(exporter_id, output_file);
+}
+
+} // namespace tecs
